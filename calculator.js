f5cbf516278e35a3ff63d1160b823cd6a7fb8ea6
Ric0la13 <ricolaviva@gmail.com>
2024-02-25 20:21:24 +0100

    href set form action to # add constant in calculator.js and add an eventListener to the calculate button and move the button out of the form

--- calculator.js
+++ calculator.js
@@ -1,2 +1,57 @@
+const anschaffungApple = 5;
+const anschaffungFairphone = 5;
+const anschaffungGoogle = 5;
+const anschaffungHuawei= 5;
+const anschaffungOppo = 5;
+const anschaffungSamsung = 5;
+const anschaffungShiftphone = 5;
+const anschaffungTranssion = 5;
+const anschaffungVivo= 5;
+const anschaffungXiaomi = 5;
+const anschaffungEineAndereMarke = 5;
+
+const unterschiedTag = 0;
+const unterschiedNacht= 5;
+const unterschiedFlugmodus = 3;
+
+const ladeverhaltenAkkustandschonen = -5;
+const ladeverhaltenLadeart = -5;
+const musikStreaming = 5;
+const videoStreaming = 5;
+const socialMediaNachrichten = 5;
+const socialMediaFotos = 5;
+const socialMediaActivity = 5;
+const surfingSuchanfragen = 5;
+const surfingPromtki= 5;
+const mailOhneAnhang = 5;
+const mailMitAnhang = 5;
+const videoCall = 5;
+const telefonieren = 5;
+const offlineGames = 5;
+const onlineGames = 5;
+const cloud = 5;
+const navigationsapp = 5;
+const trackingGesundheitsdaten = 5;
+const kontaktlosesBezahlen = 5;
+
+const reparieren = -5;
+const verkaufen = -5;
+const versorgen = 0;
+const recyclen = -5;
+const entsorgen = 5;
+
+let calculateButton = document.getElementById("calculate-button");
+
+debugger;
+calculateButton.addEventListener("click", event => {
+    debugger;
+    alert("alarm");
+});
+
+
+
+
+
+
 
 
